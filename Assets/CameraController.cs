3fc58a0d2541f38fe952f48e68a446646b0d63de
gabrielwong159 <gabrielwong159@gmail.com>
2016-12-07 19:17:21 +0800

    Added function for switching between 1 and 4 cameras

---
+++ Assets/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public int oneCamera;
+
+    public GameObject cameraFront;
+    public GameObject cameraLeft;
+    public GameObject cameraRight;
+    public GameObject cameraBack;
+
+    Camera c1;
+    Camera c2;
+    Camera c3;
+    Camera c4;
+
+    void Start() {
+        c1 = cameraFront.GetComponent<Camera>();
+        c2 = cameraLeft.GetComponent<Camera>();
+        c3 = cameraRight.GetComponent<Camera>();
+        c4 = cameraBack.GetComponent<Camera>();
+
+        if (oneCamera == 1) {
+            
+        }
+        else {
+            cameraFront.transform.position = new Vector3(1.08f, 1.25f, 2.95f);
+            cameraFront.transform.rotation = Quaternion.Euler(new Vector3(30, -150, 0));
+            c1.rect = new Rect(0.35f, 0.0f, 0.3f, 0.5f);
+
+            cameraLeft.transform.position = new Vector3(0.83f, 2.0f, -1.2f);
+            cameraLeft.transform.rotation = Quaternion.Euler(new Vector3(45, -30, 0));
+            c2.rect = new Rect(0.0f, 0.25f, 0.3f, 0.5f);
+
+            cameraRight.transform.position = new Vector3(-0.96f, 2.0f, 2.53f);
+            cameraRight.transform.rotation = Quaternion.Euler(new Vector3(45, 150, 0));
+            c3.rect = new Rect(0.7f, 0.25f, 0.3f, 0.5f);
+
+            cameraBack.transform.position = new Vector3(-0.94f, 2.0f, -1.24f);
+            cameraBack.transform.rotation = Quaternion.Euler(new Vector3(45, 30, 0));
+            c4.rect = new Rect(0.35f, 0.5f, 0.3f, 0.5f);
+        }
+
+    }
+}
---
+++ Assets/MerryGoRound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MerryGoRound : MonoBehaviour {
+    public GameObject refObj;
+    public float radius = 10f;
+    public float scale = 1f;
+
+    private Vector3 initRot;
+    private float initTime;
+
+    void Start () {
+	}
+
+    void OnEnable() {
+        initTime = Time.timeSinceLevelLoad;
+        // we need the camera to get to its starting position first
+        transform.position = radius * scale * (new Vector3(Mathf.Cos(0f), 0, Mathf.Sin(0f)));
+
+        // get camera to look at whatever we need and hae that as the focal point
+        transform.LookAt(refObj.transform); // should change this to the Vector3 centre pt of the entire sim
+        initRot = transform.rotation.eulerAngles;
+    }
+	
+	void Update () {
+        float time = Time.timeSinceLevelLoad - initTime;
+        transform.position = radius * scale * (new Vector3(Mathf.Cos(time), 0, Mathf.Sin(time)));
+
+        // converting in and out of quaternion is so annoying
+        Vector3 eulerRot = transform.rotation.eulerAngles;
+        float yRot = initRot.y - time * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(initRot.x, yRot, initRot.z);
+	}
+}
