2c7c459481925795d2fbf3d98129ebca5eb4da45
gabrielwong159 <gabrielwong159@gmail.com>
2016-12-07 19:33:26 +0800

    Added keyboard support for toggling camera mode

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-    public int oneCamera;
+    public int cameraMode;
 
     public GameObject cameraFront;
     public GameObject cameraLeft;
@@ -21,26 +21,50 @@ public class CameraController : MonoBehaviour {
         c3 = cameraRight.GetComponent<Camera>();
         c4 = cameraBack.GetComponent<Camera>();
 
-        if (oneCamera == 1) {
-            
+        cameraToggle(cameraMode);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            cameraMode = (cameraMode == 1) ? 0 : 1;
+            cameraToggle(cameraMode);
         }
-        else {
-            cameraFront.transform.position = new Vector3(1.08f, 1.25f, 2.95f);
-            cameraFront.transform.rotation = Quaternion.Euler(new Vector3(30, -150, 0));
-            c1.rect = new Rect(0.35f, 0.0f, 0.3f, 0.5f);
+    }
 
-            cameraLeft.transform.position = new Vector3(0.83f, 2.0f, -1.2f);
-            cameraLeft.transform.rotation = Quaternion.Euler(new Vector3(45, -30, 0));
-            c2.rect = new Rect(0.0f, 0.25f, 0.3f, 0.5f);
+    void cameraToggle(int mode) {
+        if (mode == 1) oneCamera();
+        else fourCamera();
+    }
 
-            cameraRight.transform.position = new Vector3(-0.96f, 2.0f, 2.53f);
-            cameraRight.transform.rotation = Quaternion.Euler(new Vector3(45, 150, 0));
-            c3.rect = new Rect(0.7f, 0.25f, 0.3f, 0.5f);
+    void oneCamera() {
+        cameraEnable(false);
 
-            cameraBack.transform.position = new Vector3(-0.94f, 2.0f, -1.24f);
-            cameraBack.transform.rotation = Quaternion.Euler(new Vector3(45, 30, 0));
-            c4.rect = new Rect(0.35f, 0.5f, 0.3f, 0.5f);
-        }
+        c1.rect = new Rect(0, 0, 1, 1);
+    }
 
+    void fourCamera() {
+        cameraEnable(true);
+
+        cameraFront.transform.position = new Vector3(1.08f, 1.25f, 2.95f);
+        cameraFront.transform.rotation = Quaternion.Euler(new Vector3(30, -150, 0));
+        c1.rect = new Rect(0.35f, 0.0f, 0.3f, 0.5f);
+
+        cameraLeft.transform.position = new Vector3(0.83f, 2.0f, -1.2f);
+        cameraLeft.transform.rotation = Quaternion.Euler(new Vector3(45, -30, 0));
+        c2.rect = new Rect(0.0f, 0.25f, 0.3f, 0.5f);
+
+        cameraRight.transform.position = new Vector3(-0.96f, 2.0f, 2.53f);
+        cameraRight.transform.rotation = Quaternion.Euler(new Vector3(45, 150, 0));
+        c3.rect = new Rect(0.7f, 0.25f, 0.3f, 0.5f);
+
+        cameraBack.transform.position = new Vector3(-0.94f, 2.0f, -1.24f);
+        cameraBack.transform.rotation = Quaternion.Euler(new Vector3(45, 30, 0));
+        c4.rect = new Rect(0.35f, 0.5f, 0.3f, 0.5f);
+    }
+
+    void cameraEnable(bool enable) {
+        c2.enabled = enable;
+        c3.enabled = enable;
+        c4.enabled = enable;
     }
 }
--- Assets/KeyboardController.cs
+++ Assets/KeyboardController.cs
@@ -10,7 +10,7 @@ public class KeyboardController : MonoBehaviour {
 
     public static int state = DEFAULT;
 
-    float triggeredTime;
+    private float triggeredTime;
 
 	void Start () {
         triggeredTime = Time.realtimeSinceStartup;
