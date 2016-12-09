8c3323b68d1c11c500fed4d511b36f847d40dce8
gabrielwong159 <gabrielwong159@gmail.com>
2016-12-10 03:15:48 +0800

    Added auto-revert to Palpatine mode

--- Assets/LightningBolt/Demo/RevertController.cs
+++ Assets/LightningBolt/Demo/RevertController.cs
@@ -4,11 +4,14 @@ using System.Collections;
 
 public class RevertController : MonoBehaviour {
 
+    private float triggered;
+
 	void Start () {
-	
+        triggered = Time.time;
 	}
 	
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Alpha0)) SceneManager.LoadScene("props");
+        if (Time.time - triggered > 10) SceneManager.LoadScene("props");
 	}
 }
